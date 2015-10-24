5b72dee1dfc94499df3ef2f052114c3db91e5540
Chen Botao <85280861@qq.com>
2015-10-25 01:17:26 +0800

    content free type  -> OK

--- func/rt.py
+++ func/rt.py
@@ -3,29 +3,29 @@ import re
 #******************************************************************************
 #对 aline["text"] 进行上下文无关的判定,返回 True 或者 False
 #******************************************************************************
-def is_content_start(aline):
+def cft_content_start(aline):
     return True if aline["text"]=="目录"or"目次" else False
-def is_content_title(aline):
+def cft_content_title(aline):
     return True if re.match('\d+\w+[．….]+',aline["text"]) else False
-def is_content_subtitle(aline):
+def cft_content_subtitle(aline):
     return True if re.match('\d+\.\d+\w+[．….]+',aline["text"]) else False
-def is_content_else(aline):
+def cft_content_else(aline):
     return True if re.match('\w+[．….]+',aline["text"]) else False
-def is_pic(aline):
+def cft_pic(aline):
     return True if re.match('图\d+\w.*',aline["text"]) else False
-def is_empty(aline):
+def cft_empty(aline):
     return True if aline["text"] == "" else False
-def is_table_name(aline):
+def cft_table_name(aline):
     return True if re.match("表\d+\.\d+\.\d+",aline["text"]) else False
-def is_md_table(aline):
+def cft_md_table(aline):
     return True if re.match("\|.*\|$",aline["text"]) else False
-def is_table(aline):
-    if is_table_name(aline) :
+def cft_table(aline):
+    if cft_table_name(aline) :
         return True
-    elif is_md_table(aline):
+    elif cft_md_table(aline):
         return True
     return False
-def is_table_grid(aline):
+def cft_table_grid(aline):
     a= re.match('┏[━┳]*┓',aline["text"])
     b= re.match('┣[━╋]*┫',aline["text"])
     c= re.match('┗[━┻]*┛',aline["text"])
@@ -33,18 +33,24 @@ def is_table_grid(aline):
         return True
     else:
         False
-def is_table_remarks(aline):
+def cft_table_remarks(aline):
     return True if re.match("注：",aline["text"]) else False
-def is_page(aline):
+def cft_page(aline):
     if re.match('\d+',aline["text"]):
-        if aline["text"] == re.match('\d+',text_lst[i]).group():
+        if aline["text"] == re.match('\d+',aline["text"]).group():
             return True
     else:
         return False
+def cft_0p0p0(aline):
+    return True if re.match('\d+\.\d+\.\d+',aline["text"]) else False
+def cft_0p0(aline):
+    return True if re.match('\d+\.\d+',aline["text"]) else False
+def cft_0(aline):
+    return True if re.match('\d+',aline["text"]) else False
 #******************************************************************************
 #对在aline_lst内的第i项aline["text"] 进行上下文有关的判定,返回 True 或者 False
 #******************************************************************************
-def is_list(aline):
+def cft_list(aline):
     return True if re.match('\d+\.\d+\.\d+.*：1.*[。；]2',aline["text"]) else False
 
 #******************************************************************************
@@ -67,14 +73,19 @@ def file_to_aline_lst(file):
     file.close()
     return lst
 def replace_aline_lst(lst,word_lst):
-    newlst=lst[:]
-    for i in range(0,len(newlst)):
-        newlst[i]["text"]=newlst[i]["raw_text"]
+    # lst=lst[:]
+    for i in range(0,len(lst)):
+        lst[i]["text"]=lst[i]["raw_text"]
         for word in word_lst:
-            if word[0] in newlst[i]["text"]:
-                newlst[i]["text"]=newlst[i]["text"].replace(word[0],word[1])
-    return newlst
-#def add_cf_type(lst):
-#    for aline in lst:
-#        aline["text"]
-#        aline["cf_type"]=
+            if word[0] in lst[i]["text"]:
+                lst[i]["text"]=lst[i]["text"].replace(word[0],word[1])
+    return lst
+def add_cft(lst,cft_con_lst):
+    def cft(aline,cft_con_lst):
+        for con in cft_con_lst:
+            if eval(con+"(aline)"):
+                return con
+        return "default"
+    for aline in lst:
+       aline["cft"]=cft(aline,cft_con_lst)
+    return lst
--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import func.rt , config.GB_ZW
 lst=func.rt.file_to_aline_lst(
     "C:\\Users\\Administrator\\Documents\\GitHub\\txt2md\\test_sample\\正文.txt")
 lst=func.rt.replace_aline_lst(lst,config.GB_ZW.replace_word_lst)
-#lst=func.rt.add_cf_type(lst)
+lst=func.rt.add_cft(lst,config.GB_ZW.cft_con_lst)
 #lst=func.rt.add_cs_type(lst)
 for aline in lst:
-    print(aline["text"])
+    print(aline["cft"]+":"+aline["text"])
+    # print(aline)
